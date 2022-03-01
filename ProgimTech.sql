90838f699ecc8891edff7d54c54f86a6ae181cf4
NargesLeylian <leylian.narges@gmail.com>
2022-02-28 19:22:13 -0500

    Update ProgimTech.sql

--- ProgimTech.sql
+++ ProgimTech.sql
@@ -1,7 +1,9 @@
 
-
+-- Create, Alter and Delete a Database
+/*
 CREATE DATABASE Testdb
 ALTER DATABASE Testdb MODIFY NAME = Testdb1
 EXEC sp_renamedb 'Testdb1','Testdb11'
 ALTER DATABASE Testdb11 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 DROP DATABASE Testdb11
+*/
