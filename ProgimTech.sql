52718d9e12baf5ea85e11c2d1f07e3cb550356ea
NargesLeylian <leylian.narges@gmail.com>
2022-02-28 21:59:45 -0500

    Update ProgimTech.sql

--- ProgimTech.sql
+++ ProgimTech.sql
@@ -7,3 +7,24 @@ EXEC sp_renamedb 'Testdb1','Testdb11'
 ALTER DATABASE Testdb11 SET SINGLE_USER WITH ROLLBACK IMMEDIATE
 DROP DATABASE Testdb11
 */
+
+--Create Table
+USE Sample1
+Go
+
+
+CREATE TABLE tblPerson
+(
+  ID INT NOT NULL PRIMARY KEY,
+  Name NVARCHAR(50),
+  GenderID INT
+)
+
+CREATE TABLE tblGender
+(
+  ID INT NOT NULL PRIMARY KEY,
+  Gender NVARCHAR(20) NOT NULL
+)
+
+ALTER TABLE tblPerson ADD CONSTRAINT tblPerson_GenderID_FK
+FOREIGN KEY (GenderID) REFERENCES tblGender(ID)
